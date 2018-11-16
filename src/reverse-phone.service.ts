aa61b3f47f56ba67e665d00b1489b8e190bf8ee5
KevinChheang <kevin.chh89@gmail.com>
2018-11-16 09:02:15 +0000

    Display more info

--- src/reverse-phone.service.ts
+++ src/reverse-phone.service.ts
@@ -8,10 +8,11 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class ReversePhoneService {
+  api_key: string = "3906d2ed4c654970839de1c863cdc9d5";
 
   constructor(public http: HttpClient) {}
   
   getData(phoneNumber: string) {
-      return this.http.get("https://proapi.whitepages.com/3.0/phone?phone=" + phoneNumber + "&api_key=3906d2ed4c654970839de1c863cdc9d5");
+      return this.http.get("https://proapi.whitepages.com/3.0/phone?phone=" + phoneNumber + "&api_key=" + this.api_key);
     }
 }
